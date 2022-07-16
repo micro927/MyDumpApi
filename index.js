5750a53193dc07b7694baa2c7bc4a7e20da7eaf2
micro927 <nsbelieve-15@hotmail.com>
2022-07-16 11:27:58 +0700

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express')
+// var axios = require('axios').default;
+var cors = require('cors');
+var app = express()
+var port = 3099
+
+// app.use(cors('*'))
+
+app.get('/', (req, res) => {
+    // console.log(req);
+    res.send('Hello World!')
+})
+
+app.get('/football/leagues', (req, res) => {
+    const leagues = JSON.parse(require("fs").readFileSync("./football/Leagues.json", "utf8"))
+    if (req) {
+        console.log('aaa');
+    }
+    res.json(leagues)
+    // res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
